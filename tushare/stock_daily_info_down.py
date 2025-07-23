e7a61f2d8cb989edce7a2a016d4315beb1b19207
兰戈 <2380584338@qq.com>
2025-07-23 21:22:53 +0800

    add: base tushare tok

--- tushare/stock_daily_info_down.py
+++ tushare/stock_daily_info_down.py
@@ -1,6 +1,6 @@
 import tushare as ts
 
-pro = ts.pro_api('6db51e15d818a75903d5783dce689ced2140014188e7feb21c5c4f04')
+pro = ts.pro_api('')
 
 df = pro.daily(ts_code='600111.SH', start_date='20200101', end_date='20250723')
 # df = pro.daily(trade_date='20250723')
